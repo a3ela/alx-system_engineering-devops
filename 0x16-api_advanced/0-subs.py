eab4ecec1d354e42b9d2ad89ab486e6df2506937
a3ela <abelsintayehu626@gmail.com>
2024-08-14 09:24:09 +0300

    proper indent spacing docstring

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    """
+    Queries the Reddit API and returns the number of
+    subscribers for a given subreddit.
+    If an invalid subreddit is given, returns 0.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
